616a01fcf27cc3bf8b562c982ab5b027e946c5a9
sachin.jagannath <sachin.jangam@zohomail.in>
2025-10-26 21:27:35 +0530

    initial commit

---
+++ game/game.go
@@ -0,0 +1,14 @@
+package game
+
+import "fmt"
+
+func WelcomeMessage() {
+	fmt.Println("Rules:")
+	fmt.Println("-> Only 2 Players are allowed")
+	fmt.Println()
+	fmt.Println("Rules:")
+	fmt.Println("-> Players take turns marking cells")
+	fmt.Println("-> First to get 3 in a row wins")
+	fmt.Println("-> Row, columns or diagonal counts")
+	fmt.Println()
+}
---
+++ main.go
@@ -0,0 +1,1 @@
+package go_tic_tac_toe
